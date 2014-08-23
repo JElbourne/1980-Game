26bb6755dfa50e5efde9827380bba81b468cc2de
JBourne <jayelbourne@gmail.com>
2014-08-22 23:50:23 -0400

    Hello World

---
+++ 1980.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.4
+# encoding: utf-8
+
+"""
+1980.py
+
+Created by Jason Elbourne on 2014-08-22.
+Copyright (c) 2014 Jason Elbourne. All rights reserved.
+"""
+
+
+def main():
+    print ("Hello 1980's Game!")
+
+
+if __name__ == "__main__":
+    main()
