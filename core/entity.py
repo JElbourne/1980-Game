eabcc5850e93e1fb108f14338685cb2fad03fd1f
JBourne <jayelbourne@gmail.com>
2014-08-24 20:59:03 -0400

    Added Entity object

---
+++ core/entity.py
@@ -0,0 +1,47 @@
+
+"""
+entity.py
+"""
+
+
+class Enitity(object):
+    def __init__(self, name, hp, lightLevel, level):
+        self.name = name
+        self.hp = hp
+        self.maxHp = hp
+        self.lightLevel = lightLevel
+        self.maxLightLevel = lightLevel
+        self.level = level
+
+        self.sprite = None
+
+
+class Character(Enitity):
+    def __init__(self, armour, speed, strength, hunger, gold, angle, **kwargs):
+        super(Character, self).__init__(**kwargs)
+        self.armour = armour
+        self.maxArmour = armour
+        self.speed = speed
+        self.strength = strength
+        self.hunger = hunger
+        self.maxHunger = hunger
+        self.gold = gold
+
+        self.angle = angle
+
+        # counters
+        self.steps = 0
+        self.kills = 0
+        self.friends = 0
+
+
+class Player(Character):
+    def __init__(self, armour, speed, strength, hunger, gold, angle, **kwargs):
+        super(Player, self).__init__(**kwargs)
+        self.armour = armour
+        self.maxArmour = armour
+        self.speed = speed
+        self.strength = strength
+        self.hunger = hunger
+        self.maxHunger = hunger
+        self.gold = gold
