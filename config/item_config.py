7528b0ef6cc9c87842cf6f26422da71a045548dd
JBourne <jayelbourne@gmail.com>
2014-09-05 23:53:20 -0400

    Added Item Config

---
+++ config/item_config.py
@@ -0,0 +1,23 @@
+
+"""
+item_config.py
+"""
+
+level_items = {
+    0: [1]
+}
+
+item_types = {
+    1: {
+        "ident": 1,
+        "description": "Torch will light up a portion of the map.",
+        "quantity": 1,
+        "value": 1,
+        "weight": 0.1,
+        "name": "Torch",
+        "hp": 1,
+        "lightLevel": 5,
+        "spriteX": 0,
+        "spriteY": 6
+    },
+}
